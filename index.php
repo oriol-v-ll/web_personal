53af2481ae37f7ef3b6bb7e774ea02e2bc9b0726
Oriol Villanova <62610722+oriol-v-ll@users.noreply.github.com>
2020-06-04 14:08:20 +0200

    Primer commit per a la meva web personal amb el esquema desenvolupant conjuntament amb companys

---
+++ index.php
@@ -0,0 +1,85 @@
+<!-- Documento PHP principal de la web www.cabesafamily.es
+
+
+@autor: Albert Gené Fernández
+@email: agene@maxitelco.com
+
+@autor: Oriol Villanova LLorens
+@email: oriol.villanova@estudiants.urv.cat
+
+principal.php v1.4
+-->
+
+<!DOCTYPE html>
+<meta charset="utf-8">
+<html lang='es'>
+<head>
+	<link rel="stylesheet" type="text/css" href="Style.css">
+	<link rel="stylesheet" type="text/css" href="Colors.css">
+</head>
+
+	<!--Funciones de javascript.-->
+
+	<script language="JavaScript">
+	
+		//Implementación del onclick de la imagen.
+		
+		
+	
+	</script>
+<body>
+	<!--######-MENU-######-->
+	
+	<div id='cssmenu'>
+		<ul>
+			<li class='profile'>
+			
+			<a class='menu' href='https://www.instagram.com/albert_g_3d/'>
+			<span>Instagram</span>
+			</a>
+			
+			
+			
+			</li>
+			<li class="Logo" ><a href='index.php'><img id="LogoImage" src="Imagenes/logo.png"></img></a></li>
+			<li class="Logo" ><div id="espaciador" ></div></li>
+			<li class='active'><a class='menu' href='#'><span>Inicio</span></a></li>
+			<li><a class='menu' href='portfolio.php'><span>Portfolio</span></a></li>
+			<li><a class='menu' href='#'><span>¿Quién soy?</span></a></li>
+			<li class='last'><a class='menu' href='#'><span>Contacto</span></a></li>
+			
+			
+		   
+		</ul>
+	</div>
+	
+	<br><br><br><br>
+	<div id="EspaciadorLogin"></div>
+	
+	<!--######-####-######-->
+	<table id="principal">
+	<tbody>
+	<tr>
+	<td id="left">
+		<img id="WIP" src="Imagenes/WIP.jpg"></img>
+			
+	</td>
+	<td id="right">
+				
+				
+			</td>
+	</tr>
+	</tbody>
+	</table>
+	
+	
+	
+	<footer>
+		<!--<p class="parrafos">Pagina work in progress...</p>-->
+</footer>
+	
+	
+	
+	
+</body>
+</html>
