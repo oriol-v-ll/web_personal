4cfc627a3b511fce4e3481bce6b907b8452a84de
Oriol Villanova <62610722+oriol-v-ll@users.noreply.github.com>
2020-06-10 12:31:01 +0200

    Implementació index.php
    Implementació diapo i petita descripció

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 @autor: Oriol Villanova LLorens
 @email: oriol.villanova@estudiants.urv.cat
 
-index.php v1.2
+index.php v1.12
 -->
 
 <!DOCTYPE html>
@@ -14,11 +14,96 @@ index.php v1.2
 	<link rel="stylesheet" type="text/css" href="Colors.css">
 </head>
 
+	<style type="text/css" media="screen">
+        .carousel {
+        	position: absolute;
+            width: 200px;
+            height: 200px;
+            right: 5px;
+            margin-top:5px;
+           
+           
+        }
+        #imagen {
+            width: 100px;
+            height: 100px;
+            background-size: cover;
+            overflow: visible;
+
+        }
+    </style>
+
 	<!--Funciones de javascript.-->
 
 	<script language="JavaScript">
 	
-		//Implementación del onclick de la imagen.
+				window.onload = function () {
+            // Variables
+            const IMAGENES = [
+                'Imagenes/foto1.jpg',
+                'Imagenes/foto2.jpg'
+            ];
+            const TIEMPO_INTERVALO_MILESIMAS_SEG = 1000;
+            let posicionActual = 0;
+            let $imagen = document.querySelector('#imagen');
+            let $botonPlay = document.querySelector('#play');
+            let $botonStop = document.querySelector('#stop');
+            let intervalo;
+
+            //Funciones
+
+            /*Funcion que cambia la foto en la siguiente posicion*/
+            function pasarFoto() {
+                if(posicionActual >= IMAGENES.length - 1) {
+                    posicionActual = 0;
+                } else {
+                    posicionActual++;
+                }
+                renderizarImagen();
+            }
+
+            /*Funcion que cambia la foto en la anterior posicion*/
+            function retrocederFoto() {
+                if(posicionActual <= 0) {
+                    posicionActual = IMAGENES.length - 1;
+                } else {
+                    posicionActual--;
+                }
+                renderizarImagen();
+            }
+
+
+
+            /*Funcion que actualiza la imagen de imagen dependiendo de posicionActual*/
+            function renderizarImagen () {
+                $imagen.style.backgroundImage = `url(${IMAGENES[posicionActual]})`;
+            }
+
+            /* Activa el autoplay de la imagen*/
+            function playIntervalo() {
+                intervalo = setInterval(pasarFoto, TIEMPO_INTERVALO_MILESIMAS_SEG);
+               
+                $botonPlay.setAttribute('disabled', true);
+                $botonStop.removeAttribute('disabled');
+
+            }
+
+            /*Para el autoplay de la imagen*/
+            function stopIntervalo() {
+                clearInterval(intervalo);
+                // Activamos los botones de control
+                
+                $botonPlay.removeAttribute('disabled');
+                $botonStop.setAttribute('disabled', true);
+            }
+
+            //Eventos. 
+            $botonPlay.addEventListener('click', playIntervalo);
+            // Iniciar
+            $botonStop.addEventListener('click', stopIntervalo);
+            renderizarImagen();
+        } 
+
 		
 		
 	
@@ -44,7 +129,7 @@ index.php v1.2
 		</ul>
 	</div>
 	
-	<br><br><br><br>
+	<br><br><br><br><br><br>
 	<div id="EspaciadorLogin"></div>
 	
 	<!--######-####-######-->
@@ -52,13 +137,44 @@ index.php v1.2
 	<tbody>
 	<tr>
 	<td id="left">
-		<img id="WIP" src="Imagenes/WIP.jpg"></img>
-			
+	<ul>
+		<li>
+		<h2>
+			Qui sóc?
+		</h2>
+			<p>
+			Em dic Oriol Villanova Llorens, tinc 21 anys, m'agrada molt la tecnolgia i el món de la ciència. Actualment estic estudiant el Grau en enginyeria de sistemes i serveis de telecomunicacions a la universitat Rovira i Virgili de Tarragona.
+				<p>
+					Quan tinc temps lliure m'agrada fer projectes de tecnolgia, com configurar servidors, programar de diferents maneres i utilitzar tota mena d'eines informatiques i de telecomunicacions.
+				</p>
+			</p>
+				<br>
+			</li>
+			<li>
+			<h2>
+			Projectes
+			</h2>
+			<p>
+			M'agrada treballar en projectes de programació independents, aquesta pàgina web és un exemple d'això i de moment el més gran que he fet personalment, ja que està hostejada en un servidor personal on tota la configuració de xarxa i de muntatge del sistema ha estat feta a mà. Tambè ha estat tota la pagina web programada a mà sense utilitzar cap tipus de plantilla. El codi està disponible <a href="https://github.com/oriol-v-ll/web_personal.git"> aquí</a>, per a utilitzar-ho per a qualsevol ús. 
+				<p>
+				Tots els projectes que no tinguin cap informació confidencial els aniré publicant al meu GitHub personal on estaran a la disposició de tothom. <a href="https://github.com/oriol-v-ll"> GitHub personal</a>
+				</p>
+			</p>
+			</li>
+	</ul>
 	</td>
 	<td id="right">
 				
-				
-			</td>
+		<img id="foto_perf" src="Imagenes/perfil.JPG"  ></img>
+    	<div class="carousel">
+    	<div id="imagen"></div>
+		<div class="controles">
+    		<button id="play">Play</button>
+    		<button id="stop" disabled>Stop</button>
+		</div>
+		</div>
+
+	</td>
 	</tr>
 	</tbody>
 	</table>
