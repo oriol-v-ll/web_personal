19c51c9edb0854fbf5dce9cfa5247e3f52b19de1
Oriol Villanova <62610722+oriol-v-ll@users.noreply.github.com>
2020-10-26 23:13:44 +0100

    Update autor.php
    Posibles cambios con firebase

--- autor.php
+++ autor.php
@@ -25,6 +25,30 @@ autor.php v1.4
 	
 	</script>
 <body>
+	<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.0.0/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyC3ksLYxhnA2vjjehkJnt2lzHNJ8-arsTY",
+    authDomain: "oriolvillanova-ff086.firebaseapp.com",
+    databaseURL: "https://oriolvillanova-ff086.firebaseio.com",
+    projectId: "oriolvillanova-ff086",
+    storageBucket: "oriolvillanova-ff086.appspot.com",
+    messagingSenderId: "552433996785",
+    appId: "1:552433996785:web:bad81ac7cb64dcb112ea50",
+    measurementId: "G-R908N0D4EL"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
 	
 	<!--######-MENU-######-->
 	
